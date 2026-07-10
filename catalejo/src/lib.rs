8f623324928f37cc086868a5c5f5255830bed66f
W. Frakchi <noreply@wafkse.dev>
2026-07-10 17:57:15 +0200

    catalejo{,-sys}: relax forbid links for `*_instead_of_*` lint family to a `deny`

--- catalejo/src/lib.rs
+++ catalejo/src/lib.rs
@@ -1,12 +1,15 @@
 #![forbid(
-    clippy::alloc_instead_of_core,
-    clippy::std_instead_of_core,
-    clippy::std_instead_of_alloc,
     clippy::inline_asm_x86_intel_syntax,
     clippy::missing_const_for_fn,
     clippy::missing_const_for_thread_local,
     missing_unsafe_on_extern,
     missing_abi
+)]
+#![deny(
+    clippy::alloc_instead_of_core,
+    clippy::std_instead_of_core,
+    clippy::std_instead_of_alloc,
+    reason = "clippy false-lints nightly-only exports"
 )]
 #![deny(missing_docs)]
 #![doc = include_str!("../README.md")]
